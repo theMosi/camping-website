5c8ceedc2d72c167b4f7fbb8a593c22e360c9b8e
Mosi <mostafawshe@gmail.com>
2023-02-21 21:09:06 +0330

    mobile-nav__menu Animation

--- scripts/app.js
+++ scripts/app.js
@@ -2,15 +2,18 @@ console.log("Naya Injoor JaHa");
 let father = document.querySelector('body');
 
 let navBtnElem = document.querySelector('.nav__btn');
+let navMenuElem = document.querySelector('.nav-menu');
 
 let navOpen = false
 
 navBtnElem.addEventListener('click', () => {
 if(navOpen){
     navBtnElem.classList.remove('nav__btn--open');
+    navMenuElem.classList.remove('nav-menu--open');
     navOpen=false;
 }else {
     navBtnElem.classList.add('nav__btn--open');
+    navMenuElem.classList.add('nav-menu--open');
     navOpen=true;
 }
 })
